64156d7c8bde538ccfe95243004ca9f62454fbb9
SAFRANI Fatima Ezzahra <fatimaezzahrasafrani@gmail.com>
2023-12-21 00:29:14 +0100

    i add the collab and topics section

--- projet_agile/src/pages/home.js
+++ projet_agile/src/pages/home.js
@@ -2,12 +2,16 @@ import React from 'react'
 import Navbar from '../components/navbar'
 import Slider from '../components/slider'
 import Footer from '../components/footer'
+import Collaboration from '../components/Collaboration'
+import Topics from '../components/Topics'
 
 function home() {
   return (
     <div>
         <Navbar/>
         <Slider/>
+        <Collaboration/>
+        <Topics/>
         <Footer/>
     </div>
   )
