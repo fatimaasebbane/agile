6d3f24f62cd5588c19d7b817a9838b14fa5a5009
SAFRANI Fatima Ezzahra <fatimaezzahrasafrani@gmail.com>
2023-12-21 12:12:17 +0100

    amelioration d affichage

--- projet_agile/src/components/slider.js
+++ projet_agile/src/components/slider.js
@@ -6,9 +6,9 @@ import '../css/slider.css';
 
 const CarouselSlider = () => {
   const backgroundImageUrls = [
-    'https://www.evenement.com/wp-content/uploads/2019/09/samuel-pereira-uf2nnANWa8Q-unsplash-2-768x415.jpg',
-    'https://i.pinimg.com/564x/e5/5b/d0/e55bd0f09f409f15fe638e507e349c1f.jpg',
-    'https://i.pinimg.com/564x/f7/e5/3c/f7e53c57dc40cfe95e1c4289a760cc0d.jpg'
+    'https://global.ariseplay.com/amg/www.thisdaylive.com/uploads/ARTIFICIAL-INTELLIGENCE.jpg',
+    'images/slide1.jfif',
+    'https://imageio.forbes.com/specials-images/imageserve/64b5825a5b9b4d3225e9bd15/artificial-intelligence--ai/960x0.jpg?height=506&width=711&fit=bounds'
   ];
 
   const calculateTimeLeft = () => {
@@ -63,9 +63,9 @@ const CarouselSlider = () => {
                 <div className="container">
                   <div className="row">
                     <div className="col-xl-8 col-lg-8 col-md-9 col-sm-10">
-                      <div className="hero__caption">
+                      <div className="hero__caption1">
                         <h1 className='titre'>{typedTitle}</h1>
-                        <div className='btn btnn'>Contact us</div>
+                        <div className='btn btnn1'>Contact us</div>
                       </div>
                     </div>
                   </div>
@@ -73,23 +73,23 @@ const CarouselSlider = () => {
               </div>
             </div>
 
-            <div className="counter-section d-none d-sm-block">
-              <div className="cd-timer" id="countdown">
+            <div className="counter-section1 d-none d-sm-block">
+              <div className="cd-timer1" id="countdown">
                 <div style={{ display: 'inline-block', margin: '0 5px' }}>
                   <span className="cd-item">{timeLeft.days}:</span>
-                  <p className="lettre">Days</p>
+                  <p className="lettre1">Days</p>
                 </div>
                 <div style={{ display: 'inline-block', margin: '0 5px' }}>
                   <span className="cd-item">{timeLeft.hours}:</span>
-                  <p className="lettre">Hrs</p>
+                  <p className="lettre1">Hrs</p>
                 </div>
                 <div style={{ display: 'inline-block', margin: '0 5px' }}>
                   <span className="cd-item">{timeLeft.minutes}:</span>
-                  <p className="lettre">Min</p>
+                  <p className="lettre1">Min</p>
                 </div>
                 <div style={{ display: 'inline-block', margin: '0 5px' }}>
                   <span className="cd-item">{timeLeft.seconds}</span>
-                  <p className="lettre">Sec</p>
+                  <p className="lettre1">Sec</p>
                 </div>
               </div>
             </div>
